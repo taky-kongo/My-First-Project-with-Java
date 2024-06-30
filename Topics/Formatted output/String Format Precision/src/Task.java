1fee94bb7ccbda8c0bccb090299bf9ef684e229c
Kongo Abdoul <abdoulmalickkongo@gmail.com>
2024-06-30 20:24:26 +0000

    Mise à jour

--- Topics/Formatted output/String Format Precision/src/Task.java
+++ Topics/Formatted output/String Format Precision/src/Task.java
@@ -2,6 +2,9 @@
 
 public class Task {
   public static void main(String[] args) {
-    // put your code here
+    System.out.print("**");
+    System.out.println();
+    System.out.println("**");
+    System.out.print("**");
   }
 }
---
+++ Topics/Reading user's input with Scanner/Print strings in the reverse order/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String str1 = scanner.next();
+        String str2 = scanner.next();
+        String str3 = scanner.next();
+
+        System.out.println(str3);
+        System.out.println(str2);
+        System.out.println(str1);
+    }
+}
